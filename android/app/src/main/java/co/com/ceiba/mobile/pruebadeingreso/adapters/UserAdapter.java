9836828de918205d238a795e81329b494340e8df
LenisDiaz <71986701+LenisDiaz@users.noreply.github.com>
2020-09-28 11:14:04 -0500

    prueba

---
+++ android/app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/adapters/UserAdapter.java
@@ -0,0 +1,195 @@
+package co.com.ceiba.mobile.pruebadeingreso.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+import co.com.ceiba.mobile.pruebadeingreso.R;
+import co.com.ceiba.mobile.pruebadeingreso.entities.Post;
+import co.com.ceiba.mobile.pruebadeingreso.entities.User;
+import co.com.ceiba.mobile.pruebadeingreso.helper.Util;
+import co.com.ceiba.mobile.pruebadeingreso.rest.Endpoints;
+import co.com.ceiba.mobile.pruebadeingreso.view.MainActivity;
+import co.com.ceiba.mobile.pruebadeingreso.view.PostActivity;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolderUser> implements Filterable {
+
+    ArrayList<User> users;
+    ArrayList<User> usersFiltered;
+
+    private Context mcontext;
+    private Activity mActivity;
+    private  ArrayList<Post> post;
+    private Gson gson;
+
+    public UserAdapter(ArrayList<User> users, Context context, Activity activity) {
+        this.users = users;
+        this.mActivity = activity;
+        this.mcontext = context;
+        this.usersFiltered = users;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderUser onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        if(usersFiltered.size()>0) {
+             view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_list_item, parent, false);
+        }else{
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.empty_view, parent, false);
+        }
+        return new ViewHolderUser(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderUser holder, int position) {
+        holder.name.setText(usersFiltered.get(position).getNombre());
+        holder.phone.setText(usersFiltered.get(position).getPhone());
+        holder.email.setText(usersFiltered.get(position).getEmail());
+
+        gson = new Gson();
+        final String user;
+        User userSelected = (User) usersFiltered.get(position);
+        user = gson.toJson(userSelected);
+        System.out.println("USER " + userSelected.getId());
+
+        final int posicion = position;
+
+        holder.post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Util.openProggressDialog(MainActivity.progressDialog,mcontext.getResources().getString(R.string.generic_message_progress));
+                OkHttpHandler handler = new OkHttpHandler();
+                try {
+                    Log.d("URL POST",Endpoints.URL_BASE + Endpoints.GET_POST_USER + usersFiltered.get(posicion).getId());
+                    handler.execute(Endpoints.URL_BASE + Endpoints.GET_POST_USER + usersFiltered.get(posicion).getId(), user).get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return usersFiltered.size();
+    }
+
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    usersFiltered = users;
+                } else {
+                    ArrayList <User> filteredList = new ArrayList<>();
+                    for (User row : users) {
+
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or phone number match
+                        if (row.getNombre().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    usersFiltered = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = usersFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                usersFiltered = (ArrayList<User>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public class ViewHolderUser extends RecyclerView.ViewHolder {
+
+        TextView name,phone,email;
+        Button post;
+        public ViewHolderUser(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.name);
+            phone = (TextView)itemView.findViewById(R.id.phone);
+            email =(TextView) itemView.findViewById(R.id.email);
+            post =(Button) itemView.findViewById(R.id.btn_view_post);
+        }
+    }
+
+    private class OkHttpHandler extends AsyncTask<String, Void, byte[]> {
+
+        OkHttpClient client = new OkHttpClient();
+        String responseService;
+        ArrayList<Util.Extra> extras = new ArrayList<>();
+        String user;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected byte[] doInBackground(String... params) {
+            Request.Builder builder = new Request.Builder();
+            builder.url(params[0]);
+            Request request = builder.build();
+            try {
+                Response response = client.newCall(request).execute();
+                ResponseBody responseBody = response.body();
+                responseService = responseBody.string();
+                Log.d("RESPONSE POST",responseService);
+                user = params[1];
+                /*Type postListType = new TypeToken<ArrayList<Post>>(){}.getType();
+                post = gson.fromJson(responseService,postListType);*/
+                return response.body().bytes();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes) {
+            super.onPostExecute(bytes);
+            try {
+                    extras.add(new Util.Extra("post",responseService));
+                    extras.add(new Util.Extra("user",user));
+                    Util.closeProggressDialog(MainActivity.progressDialog);
+                    Util.GoToNextActivityCleanStack(mActivity, PostActivity.class,false,extras);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+}
---
+++ android/app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/MainActivity.java
@@ -0,0 +1,248 @@
+package co.com.ceiba.mobile.pruebadeingreso.view;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import co.com.ceiba.mobile.pruebadeingreso.R;
+import co.com.ceiba.mobile.pruebadeingreso.adapters.UserAdapter;
+import co.com.ceiba.mobile.pruebadeingreso.entities.User;
+import co.com.ceiba.mobile.pruebadeingreso.helper.ConectionSQLiteHelper;
+import co.com.ceiba.mobile.pruebadeingreso.helper.Util;
+import co.com.ceiba.mobile.pruebadeingreso.rest.Endpoints;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+public class MainActivity extends Activity {
+
+    private OkHttpClient httpClient;
+    private Gson gson;
+    private ConectionSQLiteHelper conection;
+    private RecyclerView recyclerUsers;
+    private EditText search;
+    private  ArrayList<User> users;
+    private UserAdapter adapter;
+    public static ProgressDialog progressDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        users = new ArrayList<>();
+        recyclerUsers = (RecyclerView) findViewById(R.id.recyclerViewSearchResults);
+        conection = new ConectionSQLiteHelper(this,"bd_user",null,2);
+        progressDialog = new ProgressDialog(this);
+        this.httpClient =  new OkHttpClient();
+        this.gson = new Gson();
+
+        if(Util.veirifyConnection(this)){
+            if(verifyDB()){
+                getUsers();
+            }else{
+                showUsers(users);
+            }
+        }
+
+
+    }
+
+    public void filter (){
+        search = (EditText) findViewById(R.id.editTextSearch);
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+    }
+
+    private void getUsers (){
+
+        Util.openProggressDialog(progressDialog,getResources().getString(R.string.generic_message_progress));
+
+
+        OkHttpHandler handler = new OkHttpHandler();
+        try {
+            handler.execute(Endpoints.URL_BASE+Endpoints.GET_USERS).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private boolean saveUsers (ArrayList <User> users){
+        boolean response = false;
+        SQLiteDatabase db = conection.getWritableDatabase();
+        try{
+            for (User user: users) {
+                ContentValues valuesUser = new ContentValues();
+                valuesUser.put(Util.ATRIBUTE_USER_ID,user.getId());
+                valuesUser.put(Util.ATRIBUTE_USER_NAME,user.getNombre());
+                valuesUser.put(Util.ATRIBUTE_USER_USERNAME,user.getUsername());
+                valuesUser.put(Util.ATRIBUTE_USER_EMAIL,user.getEmail());
+                valuesUser.put(Util.ATRIBUTE_USER_PHONE,user.getPhone());
+                valuesUser.put(Util.ATRIBUTE_USER_WEBSITE,user.getWebsite());
+                db.insert(Util.TABLE_USER,Util.ATRIBUTE_USER_ID,valuesUser);
+
+                ContentValues valuesAdress = new ContentValues();
+                valuesAdress.put(Util.ATRIBUTE_ADDRESS_USERID,user.getId());
+                valuesAdress.put(Util.ATRIBUTE_ADDRESS_STREET,user.getAddress().getStreet());
+                valuesAdress.put(Util.ATRIBUTE_ADDRESS_SUITE,user.getAddress().getSuite());
+                valuesAdress.put(Util.ATRIBUTE_ADDRESS_CITY,user.getAddress().getCity());
+                valuesAdress.put(Util.ATRIBUTE_ADDRESS_ZIPCODE,user.getAddress().getZipcode());
+                db.insert(Util.TABLE_ADDRESS,Util.ATRIBUTE_ADDRESS_USERID,valuesAdress);
+
+                ContentValues valuesGeo = new ContentValues();
+                valuesGeo.put(Util.ATRIBUTE_ADDRESS_USERID,user.getId());
+                valuesGeo.put(Util.ATRIBUTE_GEO_LAT,user.getAddress().getGeo().getLat());
+                valuesGeo.put(Util.ATRIBUTE_GEO_LGN,user.getAddress().getGeo().getLng());
+                db.insert(Util.TABLE_GEO,Util.ATRIBUTE_ADDRESS_USERID,valuesGeo);
+
+                ContentValues valuesCompany = new ContentValues();
+                valuesCompany.put(Util.ATRIBUTE_COMPANY_USERID,user.getId());
+                valuesCompany.put(Util.ATRIBUTE_COMPANY_NAME,user.getCompany().getName());
+                valuesCompany.put(Util.ATRIBUTE_COMPANY_CATCHPHRASE,user.getCompany().getCatchPhrase());
+                valuesCompany.put(Util.ATRIBUTE_COMPANY_BS,user.getCompany().getBs());
+                db.insert(Util.TABLE_COMPANY,Util.ATRIBUTE_COMPANY_USERID,valuesCompany);
+            }
+            response = true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        db.close();
+        return response;
+    }
+
+    private void showUsers (ArrayList<User> users){
+        recyclerUsers.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UserAdapter(users, getApplicationContext(), this);
+        recyclerUsers.setItemAnimator(new DefaultItemAnimator());
+        filter();
+        recyclerUsers.setAdapter(adapter);
+    }
+
+    private boolean verifyDB (){
+        boolean isExist= false;
+        String id,name,username,email,street,suite,city,zipcode,lat,lng,phone,website,companyName,catchPhrase,bs;
+        try{
+            SQLiteDatabase db = conection.getWritableDatabase();
+
+            Cursor cursor = null;
+            cursor =db.rawQuery(Util.QUERY_USERS, null);
+            if (cursor != null) {
+                if (cursor.getCount() > 0) {
+                    if (cursor.moveToFirst()) {
+                        do {
+
+                            id= cursor.getString(0);
+                            name = cursor.getString(1);
+                            username = cursor.getString(2);
+                            email = cursor.getString(3);
+                            street = cursor.getString(4);
+                            suite = cursor.getString(5);
+                            city = cursor.getString(6);
+                            zipcode = cursor.getString(7);
+                            lat = cursor.getString(8);
+                            lng = cursor.getString(9);
+                            phone = cursor.getString(10);
+                            website = cursor.getString(11);
+                            companyName = cursor.getString(12);
+                            catchPhrase = cursor.getString(13);
+                            bs = cursor.getString(14);
+
+                            User.Address.Geo geo = new User.Address.Geo(lat,lng);
+                            User.Address address = new User.Address(street,suite,city,zipcode,geo);
+                            User.Company company = new User.Company(companyName,catchPhrase,bs);
+                            User user = new User(id,name,username,email,address,phone,website,company);
+                            System.out.println("Lista la vaina");
+                            users.add(user);
+
+                        } while(cursor.moveToNext());
+                    }
+                    isExist = true;
+                }
+                cursor.close();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return isExist;
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private class OkHttpHandler extends AsyncTask<String, Void, byte[]> {
+
+        OkHttpClient client = new OkHttpClient();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected byte[] doInBackground(String... params) {
+            Request.Builder builder = new Request.Builder();
+            builder.url(params[0]);
+            Request request = builder.build();
+            try {
+                Response response = client.newCall(request).execute();
+                ResponseBody responseBody = response.body();
+                String responseService = responseBody.string();
+                Log.d("RESPONSE",responseService);
+                Type userListType = new TypeToken<ArrayList<User>>(){}.getType();
+                users = gson.fromJson(responseService,userListType);
+                if (saveUsers(users)) System.out.println("INSERTO BIEN");
+
+                return response.body().bytes();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes) {
+            super.onPostExecute(bytes);
+            try {
+                showUsers(users);
+                Util.closeProggressDialog(progressDialog);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+}
